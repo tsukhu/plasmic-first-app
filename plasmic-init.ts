5463cf3f55425e2725e95d0782e05eae837e9f55
Tarun Kumar Sukhu <tksukhu@gmail.com>
2022-08-03 13:15:57 +0530

    added new components

--- plasmic-init.ts
+++ plasmic-init.ts
@@ -1,10 +1,18 @@
-import { initPlasmicLoader } from "@plasmicapp/loader-nextjs";
-
+import { initPlasmicLoader } from '@plasmicapp/loader-nextjs';
+import { Section } from './components/Section';
+import { Collapse } from './components/Collapse';
+import {
+  DataTable,
+  RestQuery,
+  TableColumn,
+  TableValue,
+} from './components/AppBuilding';
 export const PLASMIC = initPlasmicLoader({
   projects: [
     {
-      id: "jMQGkaVzFLtfni46XtNnrU",
-      token: "ofoTPOvpsmVzuuXgGbgG0pDMg6341lyzQg909oCCPNkYmT08bQmjOHGjFMcCgdJ0LzabwyJzOe4H985eSM6A",
+      id: 'jMQGkaVzFLtfni46XtNnrU',
+      token:
+        'ofoTPOvpsmVzuuXgGbgG0pDMg6341lyzQg909oCCPNkYmT08bQmjOHGjFMcCgdJ0LzabwyJzOe4H985eSM6A',
     },
   ],
 
@@ -12,8 +20,33 @@ export const PLASMIC = initPlasmicLoader({
   // For development, you can set preview to true, which will use the unpublished
   // project, allowing you to see your designs without publishing.  Please
   // only use this for development, as this is significantly slower.
-  preview: false,
+  preview: true,
 });
+
+function capitalizeFirstLetter(input: string) {
+  return input.charAt(0).toUpperCase() + input.slice(1);
+}
+
+const DEFAULT_ITEMS = [
+  {
+    name: 'John Brown',
+    age: 19,
+    address: 'New York No. 1 Lake Park',
+    tags: ['student', 'developer'],
+  },
+  {
+    name: 'Jim Green',
+    age: 42,
+    address: 'London No. 1 Lake Park',
+    tags: ['teacher'],
+  },
+  {
+    name: 'Joe Black',
+    age: 32,
+    address: 'Sidney No. 1 Lake Park',
+    tags: ['cool', 'teacher'],
+  },
+];
 
 // You can register any code components that you want to use here; see
 // https://docs.plasmic.app/learn/code-components-ref/
@@ -22,4 +55,137 @@ export const PLASMIC = initPlasmicLoader({
 // http://localhost:3000/plasmic-host).  See
 // https://docs.plasmic.app/learn/app-hosting/#set-a-plasmic-project-to-use-your-app-host
 
-// PLASMIC.registerComponent(...);
+PLASMIC.registerComponent(DataTable, {
+  name: 'DataTable',
+  props: {
+    items: {
+      type: 'array',
+      defaultValue: DEFAULT_ITEMS,
+    },
+
+    columns: {
+      type: 'slot',
+      allowedComponents: ['TableColumn'],
+      defaultValue: Object.keys(DEFAULT_ITEMS[0]).map((columnName) => ({
+        type: 'component',
+        name: 'TableColumn',
+        props: {
+          title: capitalizeFirstLetter(columnName),
+          dataIndex: columnName,
+        },
+      })),
+    },
+  },
+});
+
+PLASMIC.registerComponent(TableColumn, {
+  name: 'TableColumn',
+  parentComponentName: 'DataTable',
+  providesData: true,
+  props: {
+    // The title text to show in the column headers
+    title: {
+      type: 'string',
+      defaultValue: 'Name',
+    },
+
+    // The path for the data field to get the value from
+    // Display field of the data record, support nest path by string array
+    dataIndex: {
+      type: 'string',
+      defaultValue: 'name',
+    },
+
+    // TODO: Debug why the ctx object is empty (expected to be ctx = {tableColumn})
+    // dataIndex: {
+    //   type: "dataSelector",
+    //   data: (props, ctx) => {
+    //     console.log(">>> M dataSelector", {props, ctx});
+    //     return ctx?.tableColumns ?? {}}
+    //     ,
+    //   displayName: "Field",
+    //   description: "Field to be displayed.",
+    //   defaultValue: ["name"],
+    // },
+
+    // Plasmic - Custom column template
+    columnTemplate: {
+      type: 'slot',
+      defaultValue: {
+        type: 'vbox',
+        styles: {
+          padding: 0,
+        },
+        children: [
+          {
+            type: 'component',
+            name: 'TableValue',
+          },
+        ],
+      },
+    },
+  },
+});
+
+PLASMIC.registerComponent(TableValue, {
+  name: 'TableValue',
+  parentComponentName: 'TableColumn',
+  props: {},
+});
+
+PLASMIC.registerComponent(RestQuery, {
+  name: 'RestQuery',
+  providesData: true,
+  props: {
+    url: {
+      type: 'string',
+      defaultValue: 'https://jsonplaceholder.typicode.com/posts',
+    },
+    method: {
+      type: 'choice',
+      options: ['GET', 'POST'],
+      defaultValueHint: 'GET',
+    },
+    children: {
+      type: 'slot',
+      defaultValue: [
+        {
+          type: 'vbox',
+          children: [
+            {
+              type: 'text',
+              value: 'Insert some contents here',
+            },
+          ],
+        },
+      ],
+    },
+  },
+});
+
+PLASMIC.registerComponent(Collapse, {
+  name: 'Collapse',
+  props: {
+    title: {
+      type: 'slot',
+      defaultValue: 'This is the title',
+    },
+    children: {
+      type: 'slot',
+      defaultValue: 'This is the body',
+    },
+    previewShown: 'boolean',
+  },
+});
+
+PLASMIC.registerComponent(Section, {
+  name: 'My Section',
+  props: {
+    children: {
+      type: 'slot',
+    },
+    hideHeading: {
+      type: 'boolean',
+    },
+  },
+});
